f04382b0d121339359ecc1331f434fda2e35a7ed
David Björklund <david.bjorklund@gmail.com>
2015-06-11 21:05:36 +0200

    trim away _prelude.js

--- source-maps.js
+++ source-maps.js
@@ -2,13 +2,20 @@
           var SourceMapConsumer = require('{sourceMapPath}').SourceMapConsumer;
           var smc = new SourceMapConsumer({map});
           Error.prepareStackTrace = function (error) {
-            return error.stack.replace(/([a-z10-9\.\/]+):(\d+):(\d+)/g, function (match, file, p1, p2) {
-              var line = Number(p1);
-              var column = Number(p2);
-              var original = smc.originalPositionFor({
-                line: line,
-                column: column
-              });
-              return original.source + ':' + original.line + ':' + original.column;
-            });
+            return error.stack
+              .replace(/[a-z10-9\.\/]+:(\d+):(\d+)/g, function (match, p1, p2) {
+                var line = Number(p1);
+                var column = Number(p2);
+                var original = smc.originalPositionFor({
+                  line: line,
+                  column: column
+                });
+                return original.source + ':' + original.line + ':' + original.column;
+              })
+              .split('\n')
+              .filter(function (row) {
+                return row.indexOf('browser-pack/_prelude.js:1:0') === -1;
+              })
+              .join('\n');
+
           }
